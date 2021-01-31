b4138949c422cafd8950c82e3a16b264f6368e60
djlim98 <42141895+djlim98@users.noreply.github.com>
2021-02-01 01:21:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,10 @@ def notification():
     results = []
     for patch in patchContents:
         result = list(
-            map(lambda customer: send_message(customer["notification_id"], patch), customerIdList)
+            map(
+                lambda customer: send_message(int(customer["notification_id"]), patch),
+                customerIdList,
+            )
         )
         results.append(result)
 
